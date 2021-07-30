43f48efb220c6d53d678f733d39f7c2e8542c9b0
ittonnoshio <86090538+ittonnoshio@users.noreply.github.com>
2021-07-30 22:27:56 +0900

    投稿機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,22 @@ class PostsController < ApplicationController
   def index
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
   private
 
   def post_params
-    params.require(:post).permit(:image).merge(user_id: current_user.id)
+    params.require(:post).permit(:image, :title, :category_id).merge(user_id: current_user.id)
   end
 end
