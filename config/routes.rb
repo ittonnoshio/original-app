6c34433ba6134fb52f62d8cd2f5a144bc19e7b7e
ittonnoshio <86090538+ittonnoshio@users.noreply.github.com>
2021-08-09 16:03:53 +0900

    投稿ジャンル検索機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
+  get '/posts/searchcategory',  to: 'posts#search_category'
   resources :posts, only: [:index, :new, :create, :show] do
     collection do
       get 'search'
