63c7422598f034aaf9c7f2d5a8e7927f7378edbc
ittonnoshio <86090538+ittonnoshio@users.noreply.github.com>
2021-08-09 16:45:44 +0900

    お気に入りテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
   validates :password, format: { with: /\A[a-zA-Z0-9]+\z/, message: 'is invalid. Input half-width characters.' }
 
   has_many :posts
+  has_many :favorites
 end
